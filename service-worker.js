7486a95908c6c9706bc43132eeb09a7b37a64cfd
BacilleX <alexischaussy@proton.me>
2025-11-06 21:57:18 +0100

    ymaze-tracker-pwa-v0.7-test

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='ymaze-tracker-pwa-v0.6-test';
+const CACHE='ymaze-tracker-pwa-v0.7-test';
 const ASSETS=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 
 function notifyClientsAboutUpdate(){
