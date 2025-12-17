298c62fb6e379ebea9fe1071b685e6ab46a834bb
BacilleX <alexischaussy@proton.me>
2025-12-17 12:32:54 +0100

    feature: added a note section for each mouse and its export

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'ymaze-tracker-pwa-v1.3.2';
+const CACHE = 'ymaze-tracker-pwa-v1.3.3';
 const ASSETS = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 function notifyClientsAboutUpdate() {
