8604e4b4d034b0b7a0bbf17547883eff8b20aa2b
BacilleX <alexischaussy@proton.me>
2026-02-12 09:27:53 +0100

    feat: make timer modal user-dismissible with an OK button and improve zoom functionality (gemini)

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'ymaze-tracker-pwa-v1.3.7';
+const CACHE = 'ymaze-tracker-pwa-v1.3.8-gemini';
 const ASSETS = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 function notifyClientsAboutUpdate() {
