63f5cc77ebce5fe0f32eeffdef32855c6043f4f2
BacilleX <alexischaussy@proton.me>
2026-02-12 09:37:14 +0100

    chore: Bump service worker cache version to v1.3.9. (gemini did better than codex which was Commit 632d735)

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'ymaze-tracker-pwa-v1.3.8-gemini2';
+const CACHE = 'ymaze-tracker-pwa-v1.3.9';
 const ASSETS = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 function notifyClientsAboutUpdate() {
